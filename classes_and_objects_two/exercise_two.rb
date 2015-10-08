67cce41c36c8b3d30b5ca2dab21c4551a6912c56
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2015-10-07 20:59:31 -0400

    Finish The Classes and Objects Two Solutions

---
+++ classes_and_objects_two/exercise_two.rb
@@ -0,0 +1,49 @@
+class MyCar
+  attr_accessor :color
+  
+  attr_reader :year
+  
+  def initialize(y, c, m)
+    @year = y
+    @color = c
+    @model = m
+    @speed = 0
+  end
+  
+  def spray_paint(color)
+    self.color = color
+  end
+  
+  def speed_up(speed)
+    @speed += speed
+  end
+  
+  def break_car(speed)
+    @speed -= speed
+  end
+  
+  def current_speed
+    puts "you are now going #{@speed}"
+  end
+  
+  def shut_off
+    @speed = 0
+  end
+  
+  def self.gas_milage(miles, gallon)
+    "This car gets #{miles/gallon} miles per gallon"
+  end
+  
+  def to_s
+    puts "This is a #{color} #{year} #{@model}"
+  end
+  
+end
+
+rav = MyCar.new(2015, "White", "Rav4")
+MyCar.gas_milage(315, 20)
+puts rav
+
+
+    
+    
